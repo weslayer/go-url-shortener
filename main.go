7e2bdfae6303575550033f75f5defcd361aaf7e1
weslayer <wesleywong1245@yahoo.com>
2024-08-09 23:59:41 -0700

    final changes to backend

--- main.go
+++ main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/weslayer/go-url-shortener/handler"
+	"github.com/weslayer/go-url-shortener/store"
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context){
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "this is da message!",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
 
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
+
 	err := r.Run(":9808")
-	if err != nil{
-		panic(fmt.Sprintf("Failed to start web server - Error: %v", err))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
+
 }
